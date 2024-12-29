f0ad5bcd4d2e81b041319eb908086e7ac8917e31
Gerry <106617055+Lildjgerry@users.noreply.github.com>
2024-12-28 20:03:22 -0500

    Update install.sh
    claude ai script

--- install.sh
+++ install.sh
@@ -18,8 +18,28 @@ PACKAGES=(
     "waybar"
     "networkmanager"
     "git"
-    "base-devel" # Added to ensure necessary tools for AUR packages
+    "base-devel"
 )
+
+# Get passwords securely at the beginning
+echo "Please set your passwords:"
+read -s -p "Enter root password: " ROOT_PASSWORD
+echo
+read -s -p "Confirm root password: " ROOT_PASSWORD_CONFIRM
+echo
+if [ "$ROOT_PASSWORD" != "$ROOT_PASSWORD_CONFIRM" ]; then
+    echo "Root passwords do not match!"
+    exit 1
+fi
+
+read -s -p "Enter user password: " USER_PASSWORD
+echo
+read -s -p "Confirm user password: " USER_PASSWORD_CONFIRM
+echo
+if [ "$USER_PASSWORD" != "$USER_PASSWORD_CONFIRM" ]; then
+    echo "User passwords do not match!"
+    exit 1
+fi
 
 # Update the system clock
 timedatectl set-ntp true
@@ -27,7 +47,7 @@ timedatectl set-ntp true
 # Partition the disk
 read -p "Enter disk (e.g., /dev/sda or /dev/nvme0n1): " DISK
 
-# Detect if the disk is NVMe and set partition names accordingly
+# Rest of your disk setup remains the same...
 if [[ $DISK == *"nvme"* ]]; then
     PART1="${DISK}p1"
     PART2="${DISK}p2"
@@ -56,41 +76,37 @@ pacstrap $INSTALL_DIR ${PACKAGES[@]}
 # Generate fstab
 genfstab -U $INSTALL_DIR >> $INSTALL_DIR/etc/fstab
 
-# Chroot into the new system
-arch-chroot $INSTALL_DIR <<EOF
-
+# Modified chroot commands to use the collected passwords
+arch-chroot $INSTALL_DIR bash -c "
 # Set timezone
 ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
 hwclock --systohc
 
 # Localization
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
 # Network configuration
-echo "archlinux" > /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
+echo 'archlinux' > /etc/hostname
+echo '127.0.0.1 localhost' >> /etc/hosts
+echo '::1       localhost' >> /etc/hosts
+echo '127.0.1.1 archlinux.localdomain archlinux' >> /etc/hosts
 
 # Initramfs
 mkinitcpio -P
 
-# Set root password
-passwd
+# Set root password non-interactively
+echo 'root:${ROOT_PASSWORD}' | chpasswd
 
 # Bootloader installation
 pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Enable os-prober if dual-booting is needed
-#sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
-
-# Create a user
+# Create user and set password non-interactively
 useradd -m -G wheel $USER_NAME
-passwd
+echo '${USER_NAME}:${USER_PASSWORD}' | chpasswd
 
 # Configure sudo
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
@@ -98,14 +114,13 @@ sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 # Enable services
 systemctl enable NetworkManager
 
-# Install yay (AUR helper)
-su - $USER_NAME -c "git clone https://aur.archlinux.org/yay.git /home/$USER_NAME/yay"
-su - $USER_NAME -c "cd /home/$USER_NAME/yay && makepkg -si --noconfirm"
-EOF
+# Install yay
+su - $USER_NAME -c 'git clone https://aur.archlinux.org/yay.git /home/$USER_NAME/yay'
+su - $USER_NAME -c 'cd /home/$USER_NAME/yay && makepkg -si --noconfirm'
+"
 
 # Clone setup repository
 git clone $REPO_URL $INSTALL_DIR/home/$USER_NAME/setup
 chown -R $USER_NAME:$USER_NAME $INSTALL_DIR/home/$USER_NAME/setup
 
-# Done!
 echo "Base installation is complete. Reboot into your new system."
